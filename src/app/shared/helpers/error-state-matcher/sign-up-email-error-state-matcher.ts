ca72892617ab52968ddd68edbefca89312b6613d
Nako <Toxa23A@gmail.com>
2019-03-25 10:30:06 +0200

    Remove console.log

--- src/app/shared/helpers/error-state-matcher/sign-up-email-error-state-matcher.ts
+++ src/app/shared/helpers/error-state-matcher/sign-up-email-error-state-matcher.ts
@@ -4,7 +4,6 @@ import { FormGroupDirective, FormControl, NgForm } from '@angular/forms';
 export class SignUpEmailErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     const isSubmitted = form && form.submitted;
-    console.log('control', control, control.touched);
     return !!(control && control.invalid && (control.touched || isSubmitted));
   }
 }
